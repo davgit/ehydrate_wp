e874b6998377865a7ab5674cc38b24a6cce50ae7
Luke Danielson <luke.a.danielson@gmail.com>
2015-06-30 12:32:04 -0500

    update some hero scss

--- wp-content/themes/ehydrate/templates/heroes/main-hero.php
+++ wp-content/themes/ehydrate/templates/heroes/main-hero.php
@@ -1,10 +1,10 @@
 <div id="main-hero-home" class="main-hero">
   <div class="hero hero-image" style="background-image: url('<?php echo dist_path('images/bgs/fruit-5690--1440x960.jpg');?>');">
   	<?php /*hero-video*/ ?>
-    <div class="promo-copy container">
-      <span>Flavor By Nature.<br>Nutrition By Design.</span>
+    <div class="promo-copy promo-copy-centered pos-bottom-20pct container">
+      <span class="fw-bold-ultra">Flavor By Nature.<br>Nutrition By Design.</span>
       <hr class="copysep sep-half">
-      <a class="btn btn-green btn-xlg" href="https://e-hydrate.com/product/e-hydrate-drink-mix-sample-pack/">Try E-Hydrate for Free</a>
+      <a class="btn btn-blue btn-xlg" href="https://e-hydrate.com/product/e-hydrate-drink-mix-sample-pack/">Try E-Hydrate for Free</a>
     </div><!-- end .promo-copy -->
   </div><!-- end .hero -->
 </div><!-- end main-hero -->
